9c25be6d8a4cab7616d3f2929bc9330c6059ef07
hupengfei <hupengfei@kingsoft.com>
2017-08-23 19:26:13 +0800

    add:webpack添加css等配置

--- scripts/webpack.config.js
+++ scripts/webpack.config.js
@@ -25,6 +25,42 @@ module.exports = {
             loaders: utils.cssLoaders(),
           }
       },
+      {
+          test: /\.css$/,
+          use: [
+              'style-loader',
+              'css-loader'
+          ]
+      },
+      {
+          test: /\.less$/,
+          use: [
+              'style-loader',
+              'css-loader',
+              'less-loader'
+          ]
+      },
+      {
+          test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+              limit: 10000
+          }
+      },
+      {
+          test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+              limit: 10000
+          }
+      },
+      {
+          test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+          loader: 'url-loader',
+          options: {
+              limit: 10000
+          }
+      }
     ],
   }
 }
